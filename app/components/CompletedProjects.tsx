cc63f72ece52f5869581fc6259afd27bb7bdcaf2
Berk Ozcan <berkozc.81@gmail.com>
2025-10-21 23:46:01 +0300

    image bug fix

--- app/components/CompletedProjects.tsx
+++ app/components/CompletedProjects.tsx
@@ -6,26 +6,26 @@ const projects = [
         id: 1,
         title: "CTRL-ALT-ESCAPE",
         description: "Spare time project that is dedicated to my data structure lecturer Yusuf H. Şahin from ITU. Maze escape game that you play as my lecturer.",
-        imageUrl: '${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/yhs.png',
+        imageUrl: `${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/yhs.png`,
         projectUrl: "https://www.youtube.com/watch?v=Xyfdzd10zwA"
     },
     {
         id: 2,
         title: "Tucan'ı Kurtar",
         description: "Volunteer project for the organization 'Suyun Gözyaşları' which they are teaching the environmental awareness to primary school students.",
-        imageUrl: '${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/suyun-gozyaslari.png',
+        imageUrl: `${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/suyun-gozyaslari.png`,
         projectUrl: "#"
     },
     {
         id: 3,
         title: "Block Jam 3D Clone",
         description: "Volunteer internship project given by senior developer Hüseyin Erdoğan. Replicating the original game Block Jam 3D with clean code structure",
-        imageUrl: '${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/blockjam3d.png',
+        imageUrl: `${process.env.NEXT_PUBLIC_BASE_PATH}/completed-projects-images/blockjam3d.png`,
         projectUrl: "#"
     }
 ];
 
-export default function CurrentProjects() {
+export default function CompletedProjects() {
     return (
         <section className="py-12 sm:py-20 border-t border-gray-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
